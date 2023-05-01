b13e5bb6a341367abf27af63593d20c019520ebd
pedro henrique pompermayer <pompermayerpedrohenrique@gmail.com>
2023-05-01 19:51:29 -0300

    Add atributos da carta

--- br/game/models/Carta.java
+++ br/game/models/Carta.java
@@ -1,16 +1,13 @@
 package br.game.models;
 
+import java.util.Objects;
+
 public class Carta {
 	
+	//Atributos
 	private String nipe;
 	private String face;
+	private int valor;
 	
-	public Carta(String nipe, String face) {
-		this.nipe = nipe;
-		this.face = face;
 		
 	}
-
-}
-	
-	
