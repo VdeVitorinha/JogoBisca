c970a331a10fce7ac3706f2f2bd57468e9e4859c
FejaumDwg <art.rib@hotmail.com>
2023-05-01 15:37:25 -0300

    inicialização classe Jogador

--- br/game/models/Jogador.java
+++ br/game/models/Jogador.java
@@ -1,5 +1,21 @@
 package br.game.models;
 
+import java.util.List;
+
 public class Jogador {
+	
+	private String nome;
+	private List<Carta> cartasMao;
+	private List<Carta> acumuladas;
+	
+	public void comprarCarta() {
+		
+	}
+	
+	public void jogarCarta() {
+		
+	}
+	
+	
 
 }
