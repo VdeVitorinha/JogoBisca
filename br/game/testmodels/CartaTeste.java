c8d16db5d4d2533d3f576227039db8df68dc35fa
BrunooKuster <131623696+BrunooKuster@users.noreply.github.com>
2023-05-02 20:54:46 -0300

    Teste que verifica a primeira classe carta

--- br/game/testmodels/CartaTeste.java
+++ br/game/testmodels/CartaTeste.java
@@ -1,5 +1,20 @@
 package br.game.testmodels;
 
-public class CartaTeste {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
+import br.game.models.Baralho;
+import br.game.models.Carta;
+
+public class CartaTeste {
+	//Teste que verifica se a classe carta está funcionando e a classe baralho
+	@Test 
+	void pegarCartaCorreta() {
+		Baralho b = new Baralho();
+		Carta c = b.obterCartaUm("Copas", "2", 0);
+		assertEquals(c.getNipe(), "Copas");
+		assertEquals(c.getFaces(), "2");
+		assertEquals(c.getValor(), 0);
+	}
 }
