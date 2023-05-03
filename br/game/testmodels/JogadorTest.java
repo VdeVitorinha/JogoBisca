32d0048289d81eeab9d6c2ec95280433fbe29a1b
sotnaSelA <alessandro.santos.es@gmail.com>
2023-05-02 23:30:34 -0300

    Teste metodo compraCartas()

--- br/game/testmodels/JogadorTest.java
+++ br/game/testmodels/JogadorTest.java
@@ -4,8 +4,8 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-import br.game.models.BaralhoCopia;
-import br.game.models.JogadorCopia;
+import br.game.models.Baralho;
+import br.game.models.Jogador;
 
 public class JogadorTest {
 	//Teste realizar a primeira compra no baralho de forma ordenada.
@@ -21,8 +21,17 @@ public class JogadorTest {
 	//Teste refeito.
 		@Test
 		void comprarPrimeiraCartaSemEmbaralharDois() throws Exception{
-			BaralhoCopia b =  new BaralhoCopia();
-			JogadorCopia j = new JogadorCopia();
+			Baralho b =  new Baralho();
+			Jogador j = new Jogador();
 			j.comprarCartaAcumuladas(b);
 			assertEquals(0, j.contarPontos());
+		}
+		
+		//Testando o metodo comprarCartas() e o limite de compra de ate 3 cartas.
+		@Test
+		void limiteDecomprarCartas() throws Exception {
+			Baralho b = new Baralho();
+			Jogador j = new Jogador();
+			j.comprarCartas(b);
+		    assertEquals(3, j.tamanhoMao());
 		}
