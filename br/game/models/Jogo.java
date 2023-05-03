de6417b532fc52a6b4d3a6a3924fcf7f61eb57a2
Samuel Lopes <samuelventurini2017@outlook.com>
2023-05-03 01:01:35 -0300

    Adicionado atributos da classe Jogo

--- br/game/models/Jogo.java
+++ br/game/models/Jogo.java
@@ -1,6 +1,15 @@
 package br.game.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Jogo {
+	
+	//Atributos da classe jogo
+	//Lista de cartas na mesa
+	public static List<Carta> cartasMesa = new ArrayList<>();
+	//Cria uma lista de jogadores da partida
+	public static List<Jogador> jogadores = new ArrayList<>();
 	
 	public static void main(String[] args) {
 	
