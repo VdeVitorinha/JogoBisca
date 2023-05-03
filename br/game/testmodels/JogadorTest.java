0681c72dea34fc010eff9d9d8b9fc4247dfb2c32
sotnaSelA <alessandro.santos.es@gmail.com>
2023-05-02 21:56:23 -0300

    Teste para comprar a primeira carta do baralho de forma ordenada

--- br/game/testmodels/JogadorTest.java
+++ br/game/testmodels/JogadorTest.java
@@ -1,5 +1,21 @@
 package br.game.testmodels;
 
-public class JogadorTest {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
+import br.game.models.BaralhoCopia;
+import br.game.models.JogadorCopia;
+
+public class JogadorTest {
+	//Teste para comprar a primeira carta do 
+		//baralho de forma ordenada.
+		
+		@Test
+		void comprarPrimeiraCartaSemEmbaralhar() throws Exception{
+			Baralho b =  new Baralho();
+			Jogador j = new Jogador();
+		    j.comprarCarta(b);
+			assertEquals(0, j.contarPontosTest());
+		}
 }
