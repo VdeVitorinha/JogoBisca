ec0cc09ff8e8a5f7c96af602afb777847d16fa9c
Samuel Lopes <samuelventurini2017@outlook.com>
2023-05-03 01:11:52 -0300

    Adicionado os 4 jogadores

--- br/game/models/Jogo.java
+++ br/game/models/Jogo.java
@@ -21,5 +21,25 @@ public class Jogo {
 		//Definindo o trunfo da partida
 		Carta trunfo = b.comprarCarta();
 		System.out.println("TRUNFO: "+trunfo);
+		
+		//Criando os 4 jogadores
+		Jogador j1 = new Jogador("Romario");
+		j1.comprarCartas(b);
+		JogadorCopia j2 = new Jogador("Ronaldo");
+		j2.comprarCartas(b);
+		Jogador j3 = new Jogador("Ney");
+		j3.comprarCartas(b);
+		Jogador j4 = new Jogador("Pele");
+		j4.comprarCartas(b);
+		//JogadorCopia j5 = new JogadorCopia();
+		
+		//mostra cartas dos jogadores
+		System.out.println(j1);
+		System.out.println(j2);
+		System.out.println(j3);
+		System.out.println(j4);
+		
+		System.out.println();
+		System.out.println();
 	}
 }
