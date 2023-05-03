21578babeab2a987aed3985c4e0f988ada762882
BrunooKuster <131623696+BrunooKuster@users.noreply.github.com>
2023-05-02 21:17:21 -0300

    Teste de verificação de igualdade da classe Carta

--- br/game/testmodels/BaralhoTest.java
+++ br/game/testmodels/BaralhoTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import br.game.models.Baralho;
 import br.game.models.BaralhoCopia;
+import br.game.models.CartaCopia;
 
 public class BaralhoTest {
 	
@@ -21,4 +22,12 @@ public class BaralhoTest {
 			Baralho b = new Baralho();
 			b.comprarCarta();
 			assertEquals(51, b.tamanhoMonte());
+		}
+		//Teste de verificação de igualdade da classe Carta
+		@Test
+		void pegarCartaMonte() throws Exception{
+			Baralho b = new Baralho();
+			Carta c = b.comprarCarta();
+			assertEquals("class br.game.models.Carta", c.getClass().toString());
+		}
 }
