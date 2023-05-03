234ea2ad14efd6691b2267313082fd5fe0306d09
BrunooKuster <131623696+BrunooKuster@users.noreply.github.com>
2023-05-02 21:10:24 -0300

    Teste que verifica o tamanho do baralho

--- br/game/testmodels/BaralhoTest.java
+++ br/game/testmodels/BaralhoTest.java
@@ -1,5 +1,16 @@
 package br.game.testmodels;
 
-public class BaralhoTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
+import br.game.models.Baralho;
+
+public class BaralhoTest {
+	
+	//Teste que testa o tamanho do baralho
+	@Test
+	void tamanhoBaralho() {
+		Baralho b =  new Baralho();
+		assertEquals(52, b.tamanhoMonte());
 }
